246e9b81dd2f9dfe1add49782ce3f8711cd62d73
Lionello Lunesu <lio+git@lunesu.com>
2024-02-16 10:31:48 -0800

    fix: head tap comes from main branch [no ci]

--- Formula/defang.rb
+++ Formula/defang.rb
@@ -4,7 +4,7 @@ class Defang < Formula
   url "https://api.github.com/repos/defang-io/defang/tarball/v0.5.3"
   sha256 "531a74b4516a3c16c6d0656f6a564b5a104d1bcaf7cde8637ea5e726750b09e7"
   license "MIT"
-  head "https://github.com/defang-io/defang.git", branch: "secrets"
+  head "https://github.com/defang-io/defang.git", branch: "main"
 
   depends_on "go" => :build
 
