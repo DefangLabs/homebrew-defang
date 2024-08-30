7ffac97f4c44afe17e361e8211e5411be3cd938f
GitHub Action <action@github.com>
2024-08-30 21:26:46 +0000

    Update Homebrew formula to version v0.5.41

--- Formula/defang.rb
+++ Formula/defang.rb
@@ -1,8 +1,8 @@
 class Defang < Formula
   desc "Command-line interface for the Defang Opinionated Platform"
   homepage "https://defang.io"
-  url "https://github.com/DefangLabs/defang/archive/refs/tags/v0.5.40.tar.gz"
-  sha256 "c5cdab8ea97e2aac29171303f731e24c3fb3313e704cc17b84e8096370768d98"
+  url "https://github.com/DefangLabs/defang/archive/refs/tags/v0.5.41.tar.gz"
+  sha256 "61c558edc120b37c1c7f9fafd77d1ca8ef6fa4f04cfbb974f1f75bf1576363de"
   license "MIT"
   head "https://github.com/DefangLabs/defang.git", branch: "main"
 
